a2b848434c82459efb75cffe8ef8f2b38f1e7d24
pary2024 <paeypary@gmail.com>
2025-07-11 23:41:30 +0700

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import { ThemeProvider } from "./Colors/Themes"; // make sure path is correct
-import stores from './Stores/stores';
+import stores from './stores/stores';
 import { Provider } from 'react-redux';
 
 ReactDOM.createRoot(document.getElementById("root")).render(
